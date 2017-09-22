742299a2ac2b2ab003f70d01ec3c7a7f3d927a47
Casper Lai <casper.lai@sleepingdesign.com>
2017-09-23 00:49:35 +0800

    Fix code style

--- src/GraphQL/Builder.php
+++ src/GraphQL/Builder.php
@@ -131,7 +131,8 @@ EOF;
             if (is_string($key)) {
                 $parent = $parent ? "{$parent}.{$key}" : $key;
                 $subFilters = $this->getSubFilter($parent);
-                $query .= "{$key} {$this->buildFilters($subFilters)}{ {$this->buildFields(collect($value), $parent)} },".PHP_EOL;
+                $query .= "{$key} {$this->buildFilters($subFilters)}{ {$this->buildFields(collect($value), $parent)} },"
+                    .PHP_EOL;
                 return true;
             }
 
